f4e7a67ef72f1b57d5b4e29398adf6d517edf3b6
King Wanyama <sakaribenjamin@gmail.com>
2018-06-04 10:31:32 +0300

    Initial Commit

---
+++ Activity.py
@@ -0,0 +1,12 @@
+from  peewee import *
+from  os import path
+ROOT = path.dirname(path.realpath(__file__))
+db = SqliteDatabase(path.join(ROOT, "activities.db"))
+
+class Activity(Model):
+    name = CharField()
+    desc = CharField()
+    location = CharField()
+    class Meta:
+        database = db
+Activity.create_table(fail_silently=True)
